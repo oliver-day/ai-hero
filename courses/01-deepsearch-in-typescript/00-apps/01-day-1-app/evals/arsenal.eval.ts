02da0540c8406be122656dac5793b11bc7b35069
oliver-day <oliver.day.swe@gmail.com>
2025-07-31 21:22:42 -0700

    add: answer relevancy scorer

--- courses/01-deepsearch-in-typescript/00-apps/01-day-1-app/evals/arsenal.eval.ts
+++ courses/01-deepsearch-in-typescript/00-apps/01-day-1-app/evals/arsenal.eval.ts
@@ -5,6 +5,7 @@ import type { Message } from "ai";
 import { evalite } from "evalite";
 import { askDeepSearch } from "~/deep-search";
 import { Factuality } from "~/factuality-scorer";
+import { AnswerRelevancy } from "~/answer-relevancy-scorer";
 import { devData } from "./dev";
 import { ciData } from "./ci";
 import { regressionData } from "./regression";
@@ -46,5 +47,6 @@ evalite("Arsenal", {
       },
     },
     Factuality,
+    AnswerRelevancy,
   ],
 });
